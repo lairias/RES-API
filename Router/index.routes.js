8d4724192ded9133fa0c2e44ed583fb65ea45940
lairias <lairias@unah.hn>
2020-08-16 16:17:59 -0600

    Api Lista para Usar

--- Router/index.routes.js
+++ Router/index.routes.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const Router = express.Router()
-const {nuevoCliente}= require('../controllers/PacienteControllers')
+const express = require('express');
+const Router = express.Router();
 
+
+
+
+const {
+  nuevoPaciente,
+  ObtenerPacientes,
+  ObtenerPaciente,
+  ModificarPaciente,
+  EliminarPaciente
+} = require("../controllers/PacienteControllers");
 module.exports = function (){
     //Agramos a nueos pacientes via POST
-    Router.post('/pacientes',nuevoCliente)
+    Router.post("/pacientes", nuevoPaciente);
+    Router.get("/pacientes", ObtenerPacientes);
+    Router.get("/pacientes/:id", ObtenerPaciente);
+    Router.put("/pacientes/:id", ModificarPaciente);
+    Router.delete("/pacientes/:id", EliminarPaciente);
 
 
     return Router;
--- controllers/PacienteControllers.js
+++ controllers/PacienteControllers.js
@@ -1,6 +1,6 @@
 const Paciente  = require('../models/Paciente')
 
-exports.nuevoCliente = async (req,res, next ) => {
+exports.nuevoPaciente = async (req,res, next ) => {
     //TOdo las resuestas
     // console.log(req.body)
     const paciente = new Paciente (req.body)
@@ -14,3 +14,54 @@ exports.nuevoCliente = async (req,res, next ) => {
 }
 
 
+exports.ObtenerPacientes = async (req,res ,next )=>{
+
+    try{ 
+        const paciente = await Paciente.find({})
+        res.json(paciente);
+
+    } catch (error) {
+        console.log(error)
+        next();
+    }
+}
+
+exports.ObtenerPaciente = async (req,res,next) =>{
+
+
+    try{
+        const paciente = await Paciente.findById(req.params.id)
+        res.json(paciente)
+
+    } catch (error) {
+        console.log(error)
+        next();
+    }
+}
+
+exports.ModificarPaciente = async (req,res,next)=>{
+
+    try{
+        const paciente = await Paciente.findOneAndUpdate({_id : req.params.id}, req.body, {
+            new : true
+        })
+        res.json(paciente)
+
+    } catch (error){
+    console.log(error)
+    next();
+    }
+
+}
+
+
+
+exports.EliminarPaciente = async  (req,res,next) => {
+    try{
+        await Paciente.findOneAndDelete({_id : req.params.id});
+        res.json({Mensaje : "paciente Eliminado"})
+    } catch (error){
+        console.log(error);
+        next();
+    }
+}
